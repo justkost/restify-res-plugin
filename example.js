fdd727d012976ad5a69420eb6d0df2456d957874
Konstantin Ostrouhov <ostrouhovk@yandex.ru>
2019-07-18 02:46:26 +0700

    add useful methods to res object

--- example.js
+++ example.js
@@ -3,12 +3,24 @@ const restifyResPlugin = require('./index')
 
 const server = restify.createServer()
 
-server.use(restifyResPlugin())
+server.use(restifyResPlugin({
+  defaultErrorStatus: 500,
+}))
 
 server.get('/', (req, res) => {
-  res.send({
-    status: 'success',
-  })
+  res.ok('Data')
+})
+
+server.get('/with-meta-data', (req, res) => {
+  res.ok('Data', {page: 1})
+})
+
+server.get('/error', (req, res) => {
+  res.error('Error message')
+})
+
+server.get('/error-with-status', (req, res) => {
+  res.error(400, 'Error message')
 })
 
 server.listen(3000, function() {
--- index.js
+++ index.js
@@ -1,5 +1,38 @@
 function restifyResPlugin(options) {
+  const opt = {
+    defaultErrorStatus: 200,
+    ...options,
+  }
+
   function plugin(req, res, next) {
+    const success = (data, meta) => {
+      res.json({
+        isSuccess: true,
+        data,
+        meta
+      })
+    }
+    const fail = (...args) => {
+      if (typeof args[0] === 'number') {
+        res.status(args[0])
+        res.json({
+          isSuccess: false,
+          error: args[1],
+        })
+      } else {
+        res.status(opt.defaultErrorStatus)
+        res.json({
+          isSuccess: false,
+          error: args[0],
+        })
+      }
+    }
+
+    res.ok = success
+    res.success = success
+    res.error = fail
+    res.fail = fail
+
     return next()
   }
 
